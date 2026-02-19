265c317e2f48a152d0a0bd4e6d895c191ec1ab9b
Mr4NaniReddy <dumpabhargav22@gmail.com>
2026-02-19 17:24:37 +0530

    used hasing and encrypted the credentials and validated the data

--- src/app.js
+++ src/app.js
@@ -2,22 +2,52 @@ const express = require("express");
 const {connectDB} = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js")
+const { validateSignUpData } = require("./utils/validation.js")
+const bcrypt = require("bcrypt");
 app.use(express.json());
 
 app.post("/signup", async (req, res)=>{
-    console.log(req.body);
-    const userObj = req.body;
-
-    const user = new User(userObj);
+    //validation of data
     try{
-        await user.save();
-        res.send("User saved successfully");
+        validateSignUpData(req)
+        //encrypt the password
+        const {firstName, lastName, emailId, password} = req.body;
+        const passwordHash = await bcrypt.hash(password,10);
+        console.log(passwordHash);
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
+            await user.save();
+            res.send("User saved successfully");
     }
     catch (err){
         res.status(400).send("error saving the user:" + err.message);
     }
 
 })
+
+app.post("/login", async(req,res)=>{
+    try{
+        const {emailId, password} = req.body;
+        const user = await User.findOne({emailId:emailId});
+        if(!user){
+            throw new Error("Invalid Credentials");
+        }
+        const passwordHash = await bcrypt.compare(password, user.password);
+        if(passwordHash){
+            res.send("Login Successfull");
+        }
+        else{
+            throw new Error("Invalid Credentails");
+        }
+    }
+    catch(err){
+        res.status(400).send("Cannot Login user"+err.message);
+    }
+});
 
 app.delete("/user", async(req,res)=>{
     const userId = req.body.userId;
